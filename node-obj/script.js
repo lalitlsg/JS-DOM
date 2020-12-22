dde185c0a414fb70e57d7f93a145ddc4e4506076
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-22 17:15:49 +0530

    siblings

--- node-obj/script.js
+++ node-obj/script.js
@@ -14,3 +14,14 @@ let b = document.getElementById("name");
 // for (let i of b.children) {
 //   i.textContent = "common";
 // }
+// console.log(b.firstChild);
+// console.log(b.firstElementChild);
+// b.firstElementChild.textContent = "lsg";
+// console.log(b.lastChild);
+// console.log(b.lastElementChild);
+// b.lastChild.textContent = " last child hu";
+// console.log(b.childNodes);
+// console.log(b.lastChild.previousSibling);
+// console.log(b.lastChild.previousElementSibling);
+// console.log(b.firstChild.nextSibling);
+console.log(b.firstChild.nextElementSibling);
