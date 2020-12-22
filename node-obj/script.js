e43a81c3becb33b3ce5aa64e353cff76380395e8
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-22 21:08:55 +0530

    inserting node

--- node-obj/script.js
+++ node-obj/script.js
@@ -1,13 +1,13 @@
 // console.log(document.nodeName, document.nodeType, document.nodeValue);
 
-let l = document.getElementById("name");
+// let l = document.getElementById("name");
 // console.log(l.attributes.length);
 // for (let i of l.attributes) {
 //   console.log(i.nodeName, i.nodeValue);
 //   console.log(i.name, i.value);
 // }
 
-let b = document.getElementById("name");
+// let b = document.getElementById("name");
 // console.log(b.childNodes);
 // console.log(b.children);
 
@@ -48,18 +48,74 @@ let b = document.getElementById("name");
 // let btn = document.createElement("button");
 // btn.textContent = "Button";
 // console.log(btn);
-let move = document.getElementById("move");
+// let move = document.getElementById("move");
 // move.appendChild(btn);
 // console.log(move);
 
 // let s = document.createComment("I am comment");
 // move.appendChild(s);
 
-let btn = document.createElement("button");
-let txt = document.createTextNode("My Button");
-let cmt = document.createComment("btn comment");
-btn.appendChild(txt);
-move.appendChild(btn);
-move.appendChild(cmt);
+// let btn = document.createElement("button");
+// let txt = document.createTextNode("My Button");
+// let cmt = document.createComment("btn comment");
+// btn.appendChild(txt);
+// move.appendChild(btn);
+// move.appendChild(cmt);
 
-console.log(move);
+// console.log(move);
+
+// let df = document.createDocumentFragment();
+
+// let title = document.createElement("h2");
+// title.textContent = "Please click below btn";
+
+// let btn = document.createElement("button");
+// btn.textContent = "Click Here";
+
+// df.appendChild(title);
+// df.appendChild(btn);
+// move.appendChild(df);
+// console.log(df);
+
+// let f = document.createTextNode("I");
+// let s = document.createTextNode("AM");
+// let t = document.createTextNode("IM");
+
+// move.appendChild(f);
+// move.appendChild(s);
+// move.appendChild(t);
+
+// move.normalize();
+
+// console.log(move.childNodes);
+
+// let n = document.getElementById("div1").lastElementChild;
+// let p = document.getElementById("div2");
+// let l = p.lastElementChild;
+
+// let newbtn = document.createElement("button");
+// newbtn.textContent = "New Btn";
+
+// p.insertBefore(n, l);
+
+// console.log(p);
+
+// let a = document.getElementById("div1").lastElementChild;
+// let b = document.getElementById("div2").lastElementChild;
+// b.insertAdjacentElement("beforebegin", a);
+
+// let c = document.createElement("button");
+// c.textContent = "Added";
+
+// b.insertAdjacentElement("afterend", c);
+
+// let b = document.getElementById("div2");
+// let html = "<hr />";
+// b.insertAdjacentHTML("afterend", html);
+// b.insertAdjacentHTML("beforebegin", html);
+
+// let b = document.getElementById("div2").lastElementChild;
+// let text = "Iron Man";
+
+// b.insertAdjacentText("beforebegin", text);
+// b.insertAdjacentText("afterend", text);
