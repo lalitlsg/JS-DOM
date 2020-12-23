7b240b56e36a9ec4d9d459969fefa7a8c28d7ad5
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-23 14:08:16 +0530

    tables

---
+++ css-dom/script.js
@@ -0,0 +1,37 @@
+let table = document.getElementById("tb");
+// console.log(table.caption.textContent);
+// table.caption.innerHTML = "new";
+// table.caption.textContent = "new";
+
+// table.tHead.innerHTML = "<tr><th>Third</th><th>Forth</th></tr>";
+
+// console.log(table.tBodies[0].innerHTML);
+
+// let cap = table.createCaption();
+// cap.innerHTML = "JS caption";
+// console.log(cap);
+
+// table.deleteCaption();
+
+// let h = table.createTHead();
+// h.innerHTML = "<tr><th>MF</th><th>Stocks</th></tr>";
+// console.log(h);
+
+// table.deleteTHead();
+
+// let f = table.createTFoot();
+// console.log(f);
+
+// table.deleteTFoot();
+
+// table.insertRow(0).insertCell(0).textContent = "la";
+// table.deleteRow(2);
+
+// let body = table.lastElementChild.previousElementSibling;
+// console.log(body.rows);
+// body.insertRow(-1).insertCell(0).textContent = "body";
+
+// body.deleteRow(0);
+// body.rows[0].cells[0].innerHTML = "rajat";
+// body.rows[0].insertCell(-1).innerHTML = "ncell";
+// body.rows[0].deleteCell(0);
