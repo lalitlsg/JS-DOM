043e684d88668c4a0412b3bee1ebb22a2871578a
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-23 15:51:20 +0530

    selectors

---
+++ css-selector/script.js
@@ -0,0 +1,18 @@
+// let list = document.querySelectorAll("p");
+// console.log(list);
+
+// let list = document.querySelectorAll("p, h1");
+// console.log(list);
+
+// let list = document.querySelectorAll("*");
+
+// let list = document.querySelectorAll(".head");
+
+// let list = document.querySelectorAll("h1.head");
+// let list = document.querySelectorAll("p#p1");
+// let list = document.querySelectorAll("p[title='first'][id='p1']");
+// let list = document.querySelectorAll('[class *="he"]');
+
+let list = document.querySelectorAll(":empty");
+
+console.log(list);
