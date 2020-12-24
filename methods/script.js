a4072c6de8137b7460c13da851dda995d56d1369
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-24 13:03:41 +0530

    forms

--- methods/script.js
+++ methods/script.js
@@ -1,10 +1,10 @@
-let c = setTimeout(
-  a => {
-    console.log(a);
-  },
-  5000,
-  "Hi"
-);
+// let c = setTimeout(
+//   a => {
+//     console.log(a);
+//   },
+//   5000,
+//   "Hi"
+// );
 
 // document.getElementById("clear").addEventListener(
 //   "click",
@@ -14,18 +14,52 @@ let c = setTimeout(
 //   false
 // );
 
-let i = setInterval(
-  l => {
-    console.log(l);
-  },
-  5000,
-  "Lalit"
-);
+// let i = setInterval(
+//   l => {
+//     console.log(l);
+//   },
+//   5000,
+//   "Lalit"
+// );
 
-document.getElementById("clear").addEventListener(
-  "click",
-  () => {
-    clearInterval(i);
-  },
-  false
-);
+// document.getElementById("clear").addEventListener(
+//   "click",
+//   () => {
+//     clearInterval(i);
+//   },
+//   false
+// );
+
+// window.onload = () => {
+//   window.alert("Lalit");
+// };
+
+// window.addEventListener("load", () => {
+//   window.alert("Lalit G");
+// });
+
+// let f = document.forms[1];
+// let f = document.forms["first"];
+// let f = document.second;
+// let f = document.getElementById("first");
+
+// let e = document.first.elements[1].name;
+
+// let e = document.first.elements["pass"];
+// let e = document.first.user;
+// let e = document.first.user.value;
+
+const validate = () => {
+  let reg = /^([^0-9\W]*)$/;
+  if (reg.test(document.first.user.value)) {
+    document.first.user.style.backgroundColor = "green";
+    document.getElementById("info").textContent = "Valid";
+  } else {
+    document.first.user.style.backgroundColor = "red";
+    document.getElementById("info").textContent = "Invalid";
+  }
+};
+
+document.first.user.addEventListener("keyup", validate, false);
+
+// console.log(i);
